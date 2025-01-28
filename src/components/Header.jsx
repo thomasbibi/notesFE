ed704ee959030f114cde3e05becacfc82656a96b
thomasbibi <thomasbibin38@gmail.com>
2025-01-29 01:32:19 +0530

    changed favicon and bootstrap fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,27 @@ const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
+                {/* Brand Name */}
                 <Link className="navbar-brand" to="/">NotesApp</Link>
-                <div className="collapse navbar-collapse">
+
+                {/* Toggler for mobile view */}
+               { shouldDisplayAuthLinks && (
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+               )    
+            }
+
+                {/* Collapsible content */}
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-auto">
                         {shouldDisplayAuthLinks && (
                             <>
